94e2d096141a5b5f249b2b5a49934ed0ebd7f78c
Flynth <47421408+Flyntth@users.noreply.github.com>
2022-12-13 23:57:19 +0100

    protected routes

--- frontend/cyberfront/src/App.js
+++ frontend/cyberfront/src/App.js
@@ -8,19 +8,56 @@ import AddIngredient from './components/ingredientPage/AddIngredient';
 import IngredientPageList from './components/ingredientPage/IngredientPageList';
 import EditIngredient from './components/ingredientPage/EditIngredient';
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const AnonymousGuard = ({component : Component, ...props}) =>{
+
+  const role = localStorage.getItem('role');
+  console.log(role);
+  switch(role){
+    case 'admin':
+      return  <Navigate to='/modeles'/>;
+    case 'rd':
+      return <Navigate to='/ingredients'/>;
+
+    default:
+      return <Component {...props} />;
+  }
+}
+
+const AdminGuard = ({component : Component, ...props}) =>{
+
+  const role = localStorage.getItem('role');
+  if(role==='admin'){
+    return <Component {...props} />;
+  } else {
+    return <Navigate to='/login'/>;
+  }
+}
+
+const RdGuard = ({component : Component, ...props}) =>{
+
+  const role = localStorage.getItem('role');
+  if(role==='rd'){
+    return <Component {...props}/>;
+  } else {
+    return <Navigate to='/login'/>;
+  }
+}
 
 function App() {
+
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<LoginPage/>} />
-        <Route exact path="/modeles" element={<ModelPageList />} />
-        <Route exact path="/addModel" element={<AddModel />} />
-        <Route exact path="/editModel/:id" element={<EditModel />} />
-        <Route exact path="/ingredients" element={<IngredientPageList />} />
-        <Route exact path="/addIngredient" element={<AddIngredient />} />
-        <Route exact path="/editIngredient/:id" element={<EditIngredient />} />
+        <Route exact path="/login" element={<AnonymousGuard component={LoginPage}/>} />
+        <Route exact path="/modeles" element={<AdminGuard component={ModelPageList}/>} />
+        <Route exact path="/addModel" element={<AdminGuard component={AddModel}/>} />
+        <Route exact path="/editModel/:id" element={<AdminGuard component={EditModel}/>} />
+        <Route exact path="/ingredients" element={<RdGuard component={IngredientPageList}/>} />
+        <Route exact path="/addIngredient" element={<RdGuard component={AddIngredient}/>} />
+        <Route exact path="/editIngredient/:id" element={<RdGuard component={EditIngredient}/>} />
+        <Route exact path="*" element={<Navigate to='/login'/>} />
       </Routes>
     </div>
   );

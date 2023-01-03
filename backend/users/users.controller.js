57fa0d1c27c3b208c52998d21aebf3860a497ffe
MaximeMeh <44493799+MaximeMeh@users.noreply.github.com>
2023-01-03 11:34:35 +0100

    add role rd

--- backend/users/users.controller.js
+++ backend/users/users.controller.js
@@ -70,7 +70,7 @@ function createSchema(req, res, next) {
     const schema = Joi.object({
         firstName: Joi.string().required(),
         lastName: Joi.string().required(),
-        role: Joi.string().valid(Role.Admin, Role.User).required(),
+        role: Joi.string().valid(Role.Admin, Role.User, Role.rd).required(),
         email: Joi.string().email().required(),
         password: Joi.string().min(6).required(),
         confirmPassword: Joi.string().valid(Joi.ref('password')).required()
@@ -82,7 +82,7 @@ function updateSchema(req, res, next) {
     const schema = Joi.object({
         firstName: Joi.string().empty(''),
         lastName: Joi.string().empty(''),
-        role: Joi.string().valid(Role.Admin, Role.User).empty(''),
+        role: Joi.string().valid(Role.Admin, Role.User, Role.rd).empty(''),
         email: Joi.string().email().empty(''),
         password: Joi.string().min(6).empty(''),
         confirmPassword: Joi.string().valid(Joi.ref('password')).empty('')
